bab852f42266abe9d71cc98cba4c3a917048fa49
Apua <Apua.A.Aa@gmail.com>
2023-05-27 14:14:09 +0800

    Init 5.1

---
+++ src/esheep5.1.js
@@ -0,0 +1,105 @@
+// TODO: setup a page to check each animation defined in XML
+
+
+function parseXml(xml) {
+    const dom = (new DOMParser()).parseFromString(xml, 'text/xml');
+
+    // Collect data from dom to dict, focus on web used only
+    const dict = new Map([...dom.querySelector(':root').children].map(elm => [ elm.nodeName,
+        elm.nodeName == 'header' ?
+            new Map([...elm.children].map(elm => [ elm.nodeName,
+                elm.textContent
+            ]))
+        : elm.nodeName == 'image' ?
+            new Map([...elm.children].map(elm => [ elm.nodeName,
+                elm.textContent
+            ]))
+        : elm.nodeName == 'spawns' ?
+            [...elm.children].map(elm => {
+                const dict = new Map();
+                [...elm.attributes].forEach(attr => dict.set(attr.name, attr.value));
+                [...elm.children].forEach(elm => dict.set(elm.nodeName, elm.textContent));
+                return dict;
+            })
+        : elm.nodeName == 'childs' ?
+            [...elm.children].map(elm => {
+                const dict = new Map();
+                [...elm.attributes].forEach(attr => dict.set(attr.name, attr.value));
+                [...elm.children].forEach(elm => dict.set(elm.nodeName, elm.textContent));
+                return dict;
+            })
+        : elm.nodeName == 'animations' ?
+            [...elm.children].map(elm => {
+                const dict = new Map();
+                [...elm.attributes].forEach(attr => dict.set(attr.name, attr.value));
+                [...elm.children].forEach(elm => dict.set(elm.nodeName,
+                    elm.nodeName == 'name' ?
+                        elm.textContent
+                    : elm.nodeName == 'start' ?
+                        new Map([...elm.children].map(elm => [elm.nodeName, elm.textContent]))
+                    : elm.nodeName == 'end' ?
+                        new Map([...elm.children].map(elm => [elm.nodeName, elm.textContent]))
+                    : elm.nodeName == 'sequence' ?
+                        (() => {
+                            const dict = new Map(), frame = [], next = [];
+                            dict.set('frame', frame);
+                            dict.set('next', next);
+                            [...elm.attributes].forEach(attr => dict.set(attr.name, attr.value));
+                            [...elm.children].forEach(elm => {
+                                if (elm.nodeName == 'frame') {
+                                    frame.push(elm.textContent);
+                                } else if (elm.nodeName == 'next') {
+                                    const dict = new Map();
+                                    [...elm.attributes].forEach(attr => dict.set(attr.name, attr.value));
+                                    dict.set(elm.nodeName, elm.textContent);
+                                    next.push(dict);
+                                } else {
+                                }
+                            });
+                            return dict;
+                        })()
+                    : elm.nodeName == 'border' ?
+                        [...elm.children].map(elm => {
+                            const dict = new Map();
+                            [...elm.attributes].forEach(attr => dict.set(attr.name, attr.value));
+                            dict.set(elm.nodeName, elm.textContent);
+                            return dict;
+                        })
+                    : elm.nodeName == 'gravity' ?
+                        [...elm.children].map(elm => {
+                            const dict = new Map();
+                            [...elm.attributes].forEach(attr => dict.set(attr.name, attr.value));
+                            dict.set(elm.nodeName, elm.textContent);
+                            return dict;
+                        })
+                    : null
+                ));
+                return dict;
+            })
+        : null
+    ]));
+    return dict;
+}
+
+
+async function fromUri(xmlPath) {
+    const resp = await fetch(xmlPath);
+    return parseXml(await resp.text());
+}
+
+
+async function listPetSources() {
+    const ref = 'https://adrianotiger.github.io/desktopPet/Pets/pets.json';
+    const petSrc = folder => `https://adrianotiger.github.io/desktopPet/Pets/${folder}/animations.xml`;
+    const resp = await fetch(ref, {credentials: 'same-origin', cache: "force-cache"});
+    const json = await resp.json();
+    return new Map(json.pets.map(obj => [obj.folder, petSrc(obj.folder)]));
+}
+
+
+// Main
+(async () => {
+    await fromUri('animation.xml');
+    for (const uri of (await listPetSources()).values())
+        console.log(await fromUri(uri));
+})();
