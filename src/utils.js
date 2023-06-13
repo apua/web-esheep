cee97b4c70602201d3fffb576280b62c4c5d445f
Apua Juan <apua.juan@sifive.com>
2023-06-13 15:21:43 +0800

    Implement mutex

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+const lock = new class {
+    static locked = false;
+    async delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async acquire() {
+        const cls = this.constructor;
+        while (cls.locked) await this.delay(1000);
+        cls.locked = true;
+        return new Promise(resolve => resolve(null));
+    }
+    release() {
+        const cls = this.constructor;
+        cls.locked = false;
+    }
+};
+
+
+export function withLock(callback) {
+    return lock.acquire().then(() => callback()).finally(() => lock.release());
+}
