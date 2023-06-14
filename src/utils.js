974f7acd9ae5f459a4af2e9fa4f79a2872f941a3
Apua <Apua.A.Aa@gmail.com>
2023-06-15 00:01:41 +0800

    Implement sync queue which is lockless

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,27 @@
+const sync = {
+    jobId: null,
+    queue: [],
+    trigger: () => {
+        if (sync.jobId === null && sync.queue.length)
+            sync.jobId = setTimeout(async () => {
+                const {callback, args, elm} = sync.queue.shift();
+                const result = await callback(...args);
+                elm.dispatchEvent(new CustomEvent('result', {detail: result}));
+                sync.jobId = null;
+                sync.trigger();
+            });
+    },
+};
+
+
+export const withSync = callback => (...args) => {
+    const elm = document.createElement('br');
+    sync.queue.push({callback: callback, args: args, elm: elm});
+    sync.trigger();
+    return new Promise(resolve => elm.addEventListener('result', resolve)).then(event => event.detail);
+}
+
+
 const lock = {
     isLocked: false,
     acquire: async () => {
@@ -10,20 +34,20 @@ const lock = {
 };
 
 
-export const asyncWithLock = callback => async (...args) => {
+export const withLock = callback => async (...args) => {
     await lock.acquire();
     try {
-        return await callback(...args);
+        return callback(...args);
     } finally {
         lock.release();
     }
 };
 
 
-export const asyncWithDisabled = callback => async event => {
+export const withDisabled = callback => event => {
     event.target.toggleAttribute('disabled');
     try {
-        return await callback(event);
+        return callback(event);
     } finally {
         event.target.toggleAttribute('disabled');
     }
